76292c8676838a32e58a50a6946372314990b33b
vmao19 <victormao98@gmail.com>
2021-06-27 18:22:17 -0400

    Add Animation to Home Component
    Add fade-in animation to home component

--- victor-mao-angular/src/app/components/home/home.component.ts
+++ victor-mao-angular/src/app/components/home/home.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { SocialMediaItem } from '../../models/social-media-item';
+import {trigger, style, animate, transition, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('fade', [ 
+      transition(':enter', [
+          style({ opacity: 0 }),
+          animate('1000ms ease-in', style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('fadeStaggered', [ 
+      transition(':enter', [
+        query('*', [
+          style({ opacity: 0 }),
+          stagger('200ms', [
+            animate('1000ms ease-in', style({ opacity: 1 }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
