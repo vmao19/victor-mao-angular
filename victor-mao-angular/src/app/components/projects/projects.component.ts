512c991cb8f9a44c5b317cc83dc9dea4a5ce1bb3
vmao19 <victormao98@gmail.com>
2021-07-10 20:04:00 -0400

    Add project photos

--- victor-mao-angular/src/app/components/projects/projects.component.ts
+++ victor-mao-angular/src/app/components/projects/projects.component.ts
@@ -15,49 +15,49 @@ export class ProjectsComponent implements OnInit {
       title: 'Personal Website v2',
       subtitle: 'June 2021',
       description: '',
-      image: '',
-      tech: ['Angular', 'HTML', 'CSS']
+      image: '../../../assets/project-images/raspberry-pi-scaled.jpg',
+      tech: ['Angular', 'HTML', 'CSS', 'Raspberry Pi']
     },
     {
       title: 'State Farm - Drivelytics 2.0',
       subtitle: 'Spring 2019, 16 Weeks',
       description: '',
-      image: '',
+      image: '../../../assets/project-images/capstone-scaled.jpg',
       tech: ['Swift']
     },
     {
       title: 'Android Contact Manager',
       subtitle: '2018',
       description: '',
-      image: '',
+      image: '../../../assets/project-images/contact-manager-scaled.jpg',
       tech: ['Android', 'Java']
     },
     {
       title: 'Personal Website v1',
       subtitle: 'Aug 2019',
       description: '',
-      image: '',
-      tech: ['HTML', 'CSS']
+      image: '../../../assets/project-images/personal-website-scaled.jpg',
+      tech: ['HTML', 'CSS', 'Raspberry Pi']
     },
     {
       title: 'Stroopsanity',
       subtitle: 'Oct 2015 - Feb 2016',
       description: '',
-      image: '',
+      image: '../../../assets/project-images/stroopsanity-scaled.jpg',
       tech: ['C#', 'Unity']
     },
     {
       title: 'Boggle',
       subtitle: '2012',
       description: '',
-      image: '',
+      image: '../../../assets/project-images/boggle-scaled.jpg',
       tech: ['VB.NET']
     },
     {
       title: 'Hangmaster Dynasty',
       subtitle: '2012',
       description: '**TEST** TEST *TEST*',
-      image: '',
+      image: '../../../assets/project-images/hangman-scaled.jpg',
       tech: ['VB.NET']
     },
   ];
